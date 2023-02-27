8a4404162fd67a3d10796c3809a8f8542f052846
Daniel Lear <dlear.mcsc@gmail.com>
2023-02-27 07:06:19 -0500

    Adding guild and member support to the bot

--- Automatic tierlist/tierlist.py
+++ Automatic tierlist/tierlist.py
@@ -72,6 +72,7 @@ for key, value in dict1.items():
     name, age, course = value
     print("{:<10} {:<10} {:<10}".format(name, age, course)) '''
 
+ # THIS CODE NEEDS TO BE FIXED 
 
 # bot.py
 import os
@@ -81,17 +82,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    
+        
+        print(
+            f'{client.user} is connectecd to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+            )
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
 
 client.run(TOKEN)
+
+
